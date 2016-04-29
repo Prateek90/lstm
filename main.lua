cd51160b57a42b688495e1d8c1c4ac8f58609909
Prateek90 <pc1899@nyu.edu>
2016-04-29 18:01:00 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -87,7 +87,8 @@ model = {}
     --local next_h           = nn.CMulTable()({out_gate, nn.Tanh()(next_c)})
     
     --local res=torch.Tensor():resizeAs(in_gate):fill(1)
-    local newin_gate    = nn.CSubTable()({1,in_gate})         
+    local res= nn.Copy(in_gate):fill(1)
+    local newin_gate    = nn.CSubTable()({res,in_gate*-1})         
     local next_h        = nn.CAddTable()({
         nn.CMulTable()({prev_h,newin_gate}),
         nn.CMulTable()({in_transform, newin_gate})        

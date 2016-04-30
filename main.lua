68b2247df59b9e1829930be0e2e38bfd4bd53cb0
Prateek90 <pc1899@nyu.edu>
2016-04-30 19:57:40 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -227,7 +227,7 @@ function bp(state)
         local y = state.data[state.pos + 1]
         local s = model.s[i - 1]
         -- Why 1?
-        local pred=transfer_data(torch.zeros(1)
+        local pred=transfer_data(torch.zeros(1))
         local derr = transfer_data(torch.ones(1))
         -- tmp stores the ds
         local tmp = model.rnns[i]:backward({x, y, s},

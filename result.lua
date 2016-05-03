fccbef5d8ac54dde38f4b05dd1297cdc8ed00b09
Prateek90 <pc1899@nyu.edu>
2016-05-03 14:48:31 -0400

    Added files via upload

---
+++ result.lua
@@ -0,0 +1,103 @@
+require ('nngraph')
+
+local stringx = require('pl.stringx')
+local file = require('pl.file')
+
+local ptb_path = "./data/"
+
+local testfn = ptb_path .. "ptb.test.txt"
+
+local vocab_idx = 0
+local vocab_map = {}
+
+
+local inverse_vocab_map={}
+
+local function load_data(fname)
+    local data = file.read(fname)
+    data = stringx.replace(data, '\n', '<eos>')
+    data = stringx.split(data)
+    --print(string.format("Loading %s, size of data = %d", fname, #data))
+    local x = torch.zeros(#data)
+    for i = 1, #data do
+        if vocab_map[data[i]] == nil then
+            vocab_idx = vocab_idx + 1
+            vocab_map[data[i]] = vocab_idx
+            inverse_vocab_map[vocab_idx] = data[i]
+        end
+        x[i] = vocab_map[data[i]]
+    end
+    return x
+end
+
+
+function g_enable_dropout(node)
+    if type(node) == "table" and node.__typename == nil then
+        for i = 1, #node do
+            node[i]:apply(g_enable_dropout)
+        end
+        return
+    end
+    if string.match(node.__typename, "Dropout") then
+        node.train = true
+    end
+end
+
+function g_replace_table(to, from)
+    assert(#to == #from)
+    for i = 1, #to do
+        to[i]:copy(from[i])
+    end
+end
+
+function g_disable_dropout(node)
+    if type(node) == "table" and node.__typename == nil then
+        for i = 1, #node do
+            node[i]:apply(g_disable_dropout)
+        end
+        return
+    end
+    if string.match(node.__typename, "Dropout") then
+        node.train = false
+    end
+end
+
+function reset_state(state)
+    state.pos = 1
+    if model ~= nil and model.start_s ~= nil then
+        --for d = 1, 2 * params.layers do
+        for d = 1, params.layers do
+            model.start_s[d]:zero()
+        end
+    end
+end
+
+function run_test()
+    reset_state(state_test)
+    g_disable_dropout(model.rnns)
+    local perp = 0
+    local len = state_test.data:size(1)
+    
+    -- no batching here
+    g_replace_table(model.s[0], model.start_s)
+    for i = 1, (len - 1) do
+        local x = state_test.data[i]
+        local y = state_test.data[i + 1]
+        perp_tmp, model.s[1] = unpack(model.rnns[1]:forward({x, y, model.s[0]}))
+        perp = perp + perp_tmp[1]
+        g_replace_table(model.s[0], model.s[1])
+    end
+    print("Test set perplexity : " .. g_f3(torch.exp(perp / (len - 1))))
+    g_enable_dropout(model.rnns)
+end
+	
+state_test = {data=ptb.testdataset(params.batch_size)}
+
+local states = state_test
+ 
+for _, state in pairs(states) do
+    reset_state(state)
+end
+
+
+run_test()

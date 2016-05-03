f4cdd098b4080eb45f518f0b999a13f05b0bb3cb
Prateek90 <pc1899@nyu.edu>
2016-05-02 22:02:31 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -351,7 +351,7 @@ while epoch < params.max_max_epoch do
              ', since beginning = ' .. since_beginning .. ' mins.')
          
         print('saving core')
-        torch.save('word_core.net',model.core_network)
+        torch.save('core.net',model.core_network)
         print('saved core')
     end
     
